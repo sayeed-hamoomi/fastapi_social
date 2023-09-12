5b4fcbf7964e3060a4c22f4ea4f8a47958b6450e
sayeed-hamoomi <sayeedhamoomi653@gmail.com>
2023-09-12 19:06:28 +0530

    cors initial

--- app/main.py
+++ app/main.py
@@ -11,16 +11,30 @@ from sqlalchemy.orm import Session
 from app import models, schemas
 from app.database import engine, get_db
 from app.routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5432",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
 
 """ while True:
     try:
